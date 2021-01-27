cb9329fa598b3cb9cc8ea5d454e4c51db5302506
ivan <ivanrubioangeles@gmail.com>
2021-01-27 17:42:35 -0600

    cart item elements

--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -12,6 +12,36 @@ type Props = {
 };
 
 //When creating a functional component in TypeScript we first need to use the type React.FC , which is based on the FunctionComponent interface: React. FC makes sure the signature of our function is correct and the return value is valid JSX
-const CartItem: React.FC<Props> = () => <div>Cart Item</div>;
+const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => (
+  <Wrapper>
+    <div>
+      <h3>{item.title}</h3>
+      <div className="information">
+        <p>price: ${item.price}</p>
+        <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+      </div>
+      <div className="buttons">
+        <Button
+          size="small"
+          disableElevation //to not have drop shadow
+          variant="contained" //display with a background in the buttom
+          onClick={() => removeFromCart(item.id)}
+        >
+          -
+        </Button>
+        <p>{item.amount}</p>
+        <Button
+          size="small"
+          disableElevation //to not have drop shadow
+          variant="contained" //display with a background in the buttom
+          onClick={() => addToCart(item)}
+        >
+          +
+        </Button>
+      </div>
+    </div>
+    <img src="item.image" alt={item.title} />
+  </Wrapper>
+);
 
 export default CartItem;
