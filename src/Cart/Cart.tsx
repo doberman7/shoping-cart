4cf55c3551af7b7f9b8c201484eeb4e1470d0b0b
ivan <ivanrubioangeles@gmail.com>
2021-01-27 16:58:40 -0600

    cart advance

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -16,7 +16,12 @@ const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
       <h2>Shoping Cart</h2>
       {cartItems.length === 0 ? <p>no items</p> : null}
       {cartItems.map((item) => (
-        <CartItem />
+        <CartItem
+          key={item.id}
+          item={item}
+          addToCart={addToCart}
+          removeFromCart={removeFromCart}
+        />
       ))}
     </Wrapper>
   );
--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -4,7 +4,14 @@ import { CartItemType } from "../App";
 // Styles
 import { Wrapper } from "./CartItem.styles";
 
+//tipos
+type Props = {
+  item: CartItemType;
+  addToCart: (clickedItem: CartItemType) => void;
+  removeFromCart: (id: number) => void;
+};
+
 //When creating a functional component in TypeScript we first need to use the type React.FC , which is based on the FunctionComponent interface: React. FC makes sure the signature of our function is correct and the return value is valid JSX
-const CartItem: React.FC = () => <div>Cart Item</div>;
+const CartItem: React.FC<Props> = () => <div>Cart Item</div>;
 
 export default CartItem;
